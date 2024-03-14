2ad841dce2453b3fe81accc52479a9ea7d751a2a
Joel <jreiher60@insgabrielamistral.cat>
2024-03-14 21:01:27 +0100

    Scanner cerrado, documentado, con nombres de los archivos, comentario genérico y comentarios javadoc. Le falta amplación...

--- Exercici2/src/main/java/org/GerardJoel/Main.java
+++ Exercici2/src/main/java/org/GerardJoel/Main.java
@@ -6,6 +6,14 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/*
+Aquest programa fa que llegeixi un fitxer, mostri els noms i calculi la mitja de l’edat i de les estatures
+(transformant els doubles a decimals %.2f ), mostrant-les per pantalla i desant-les en un altre arxiu de sortida.  L’estructura de l’arxiu es la següent:
+Joan 22 1.77
+Luís 22 1.80
+Pedro 20 1.73
+ */
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -20,7 +28,7 @@ public class Main {
             System.out.printf("Mitja Altura: %.2f\n", mitjes[1]);
 
             // Escriure les mitjanes a l'arxiu de sortida
-            escriureMitjaNouf(nouf, mitjes);
+            escriureMitjaNouf(nouf, mitjes, f);
         } catch (IOException e) {
             System.out.println("ERROR, el programa ha explotat1");
         }
@@ -33,12 +41,6 @@ public class Main {
      * @throws IOException
      */
     public static double[] calculaMitja(File f) throws IOException {
-        /*
-        El 'throw' serveix per evitar haver de posar el try-catch. En intentar posar el try-catch vam tenir problemes per fer que el programa pogués
-        retornar la mitjana d'anys i alçada, així que vam optar per utilitzar el 'throw'.
-        El que fa és llençar IOException amb double[] mitja = calculaMitja(f); sense la necessitat de crear un try-catch dins de la pròpia funció.
-        (Sugerencia de IntelliJ).
-         */
         Scanner scanner = new Scanner(f);
 
         int totalAnys = 0;
@@ -64,6 +66,9 @@ public class Main {
         double mitjaAnys = (double) totalAnys / compte;
         double mitjaAltura = totalAltura / compte;
 
+        // Cerrar el Scanner
+        scanner.close();
+
         // Retornar mitjanes en un array
         return new double[]{mitjaAnys, mitjaAltura};
     }
@@ -75,15 +80,27 @@ public class Main {
      * @param nouf
      * @param mitja
      */
-    public static void escriureMitjaNouf(File nouf, double[] mitja) {
+    public static void escriureMitjaNouf(File nouf, double[] mitja, File f) {
         try (PrintWriter w = new PrintWriter(nouf)) {
+            // Abrir el Scanner per llegir els noms
+            Scanner scanner = new Scanner(f);
+
+            // Escriure nombres y mitjanes en l'arxiu de sortida
+            while (scanner.hasNext()) {
+                String nom = scanner.next();
+                // %s fa que el printf sigui tractat com a una cadena de caràcters.
+                w.printf("Nom: %s\n", nom);
+            }
             // el '%.2f' fa que el nombre de punt flotant sigui de 2 decimals.
             w.printf("Mitja d'Anys: %.2f\n", mitja[0]);
             w.printf("Mitja d'Altura: %.2f\n", mitja[1]);
+
+            scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("ERROR, el programa ha explotat2");
         }
     }
 }
+
 
 
